977be1b5cdede0fc95b81a00cdb3843a692efe3b
Székely Mátyás <szekely.m4ty4s@gmail.com>
2024-04-07 11:31:39 +0200

    Add project files.

---
+++ WordChain/Node.cs
@@ -0,0 +1,18 @@
+﻿// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+// Configure the HTTP request pipeline.
+
+namespace WordChain;
+
+public class Node
+{
+    public HashSet<Edge> OutGoing { get; } = new HashSet<Edge>();
+}
+
+public class Edge
+{
+    public required Node From { get; init; }
+    public required Node To { get; init; }
+}
